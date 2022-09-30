b9bb6a00b94dfc7a20a409eda9892c7378dccc95
Arda Aydemir <arda.aydemir@kcl.ac.uk>
2022-09-30 16:37:07 +0100

    list of primes solution

--- primes.py
+++ primes.py
@@ -2,5 +2,23 @@
 """ENTER YOUR SOLUTION HERE!"""
 
 def primes(number_of_primes):
+    if number_of_primes <= 0:
+        raise ValueError(f"{number_of_primes}should be greater than 0")
     list = []
+    num = 2
+    count = 0
+    while count < number_of_primes:
+        if is_prime(num):
+            list.append(num)
+            count += 1
+            num +=1
+        else:
+            num +=1
     return list
+
+
+def is_prime(n):
+    for i in range(2,n):
+        if (n%i) == 0:
+            return False
+    return True
